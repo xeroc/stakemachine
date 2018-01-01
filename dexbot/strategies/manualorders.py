d4d019527a85a560bfb56c3caae152f40ed89c7b
Mika Koivistoinen <koivistoinen.mika@gmail.com>
2018-01-01 20:24:06 +0200

    Add base for the manual order strategy

---
+++ dexbot/strategies/manualorders.py
@@ -0,0 +1,10 @@
+from dexbot.basestrategy import BaseStrategy
+import logging
+log = logging.getLogger(__name__)
+
+
+class ManualOrders(BaseStrategy):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # TODO: do the strategy
