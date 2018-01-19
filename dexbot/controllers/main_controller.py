995ae94c9ddba2ef43c25719fe842b23532e1378
Mika Koivistoinen <mika@codaone.fi>
2018-01-19 14:38:07 +0200

    Add config fetching functions

--- dexbot/controllers/main_controller.py
+++ dexbot/controllers/main_controller.py
@@ -25,9 +25,49 @@ class MainController(object):
         # Todo: cancell all orders on removal
         self.bots[botname].terminate()
 
+    @staticmethod
+    def get_unique_bot_name():
+        """
+        Returns unique bot name "Bot %n", where %n is the next available index
+        """
+        bots = MainController.get_bots_data().keys()
+        bot = ''
+        index = 1
+        while not bot:
+            if "Bot {0}".format(index) not in bots:
+                bot = "Bot {0}".format(index)
+            index += 1
+
+        return bot
+
+    @staticmethod
+    def get_bots_data():
         """
         Returns dict of all the bots data
         """
         with open('config.yml', 'r') as f:
             yaml = YAML()
             return yaml.load(f)['bots']
+
+    @staticmethod
+    def get_latest_bot_config():
+        """
+        Returns config file data with only the latest bot data
+        """
+        with open('config.yml', 'r') as f:
+            yaml = YAML()
+            config = yaml.load(f)
+            latest_bot = list(config['bots'].keys())[-1]
+            config['bots'] = {latest_bot: config['bots'][latest_bot]}
+            return config
+
+    @staticmethod
+    def get_bot_config(botname):
+        """
+        Returns config file data with only the data from a specific bot
+        """
+        with open('config.yml', 'r') as f:
+            yaml = YAML()
+            config = yaml.load(f)
+            config['bots'] = {botname: config['bots'][botname]}
+            return config
