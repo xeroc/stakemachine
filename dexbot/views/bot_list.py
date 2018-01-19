a7097c33a3cbe1a941dd3b722db4adab069f10e8
Mika Koivistoinen <mika@codaone.fi>
2018-01-19 14:52:55 +0200

    Add multiple different gui features
    - Add botname to the bot creation dialog
    - Add bot widget ui setters
    - Change the way widgets area loaded

--- dexbot/views/bot_list.py
+++ dexbot/views/bot_list.py
@@ -2,11 +2,14 @@ from PyQt5 import QtGui, QtWidgets, QtCore
 
 from dexbot.queue.queue_dispatcher import ThreadDispatcher
 from dexbot.views.gen.bot_list_window import Ui_MainWindow
-from dexbot.views.gen.bot_item_widget import Ui_widget
 from dexbot.views.create_bot import CreateBotView
+
+from dexbot.views.bot_item import BotItemWidget
 
 
 class MainView(QtWidgets.QMainWindow):
+
+    bot_widgets = dict()
 
     def __init__(self, main_ctrl):
         self.main_ctrl = main_ctrl
@@ -17,29 +20,47 @@ class MainView(QtWidgets.QMainWindow):
 
         self.ui.add_bot_button.clicked.connect(self.handle_add_bot)
 
+        # Load bot widgets from config file
         bots = main_ctrl.get_bots_data()
-        for bot in bots:
-            self.add_bot_widget()
+        for botname in bots:
+            config = self.main_ctrl.get_bot_config(botname)
+            self.add_bot_widget(botname, config)
 
-    def add_bot_widget(self):
-        widget = BotItemWidget()
-        self.bot_container.addWidget(widget)
         # Dispatcher polls for events from the bots that are used to change the ui
         self.dispatcher = ThreadDispatcher(self)
         self.dispatcher.start()
+
+    def add_bot_widget(self, botname, config):
+        widget = BotItemWidget(botname, config, self.main_ctrl)
         widget.setFixedSize(widget.frameSize())
+        self.bot_container.addWidget(widget)
+        self.bot_widgets['botname'] = widget
 
     def handle_add_bot(self):
         create_bot_dialog = CreateBotView(self.main_ctrl)
         return_value = create_bot_dialog.exec_()
 
+        # User clicked save
         if return_value == 1:
-            self.add_bot_widget()
+            botname = create_bot_dialog.botname
+            config = self.main_ctrl.get_bot_config(botname)
+            self.add_bot_widget(botname, config)
 
     def refresh_bot_list(self):
         pass
 
+    def set_bot_name(self, bot_id, value):
+        self.bot_widgets[bot_id].set_bot_name(value)
 
+    def set_bot_account(self, bot_id, value):
+        self.bot_widgets[bot_id].set_bot_account(value)
 
+    def set_bot_profit(self, bot_id, value):
+        self.bot_widgets[bot_id].set_bot_profit(value)
 
+    def set_bot_market(self, bot_id, value):
+        self.bot_widgets[bot_id].set_bot_market(value)
 
+    def customEvent(self, event):
+        # Process idle_queue_dispatcher events
+        event.callback()
--- dexbot/views/create_bot.py
+++ dexbot/views/create_bot.py
@@ -5,17 +5,33 @@ from dexbot.views.gen.create_bot_window import Ui_Dialog
 
 class CreateBotView(QtWidgets.QDialog):
 
-    def __init__(self, model, main_ctrl):
-        self.model = model
-        self.main_ctrl = main_ctrl
+    def __init__(self, controller):
+        self.controller = controller
         super(CreateBotView, self).__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        botname = controller.get_unique_bot_name()
+        self.ui.botname_input.setText(botname)
+
         self.ui.save_button.clicked.connect(self.handle_save)
         self.ui.cancel_button.clicked.connect(self.handle_cancel)
 
+    def validate_form(self):
+        return True
+
     def handle_save(self):
+        if not self.validate_form():
+            # Todo: add validation error notice for user
+            return
+        self.botname = self.ui.botname_input.getText()
+        bot_data = {
+            'account': self.ui.account_input,
+            'market': '',
+            'module': '',
+            'bot': ''
+        }
+        self.controller.add_bot_config(self.botname, bot_data)
         self.accept()
 
     def handle_cancel(self):
