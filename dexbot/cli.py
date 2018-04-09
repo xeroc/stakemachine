499441a7f02c730bd8197d61b2a8f659fef2addf
Mika Koivistoinen <mika@codaone.fi>
2018-04-09 15:13:06 +0300

    Change pidfile removal logic

--- dexbot/cli.py
+++ dexbot/cli.py
@@ -76,33 +76,35 @@ def run(ctx):
         with open(ctx.obj['pidfile'], 'w') as fd:
             fd.write(str(os.getpid()))
     try:
+        worker = WorkerInfrastructure(ctx.config)
+        # Set up signalling. do it here as of no relevance to GUI
+        kill_workers = worker_job(worker, worker.stop)
+        # These first two UNIX & Windows
+        signal.signal(signal.SIGTERM, kill_workers)
+        signal.signal(signal.SIGINT, kill_workers)
         try:
-            worker = WorkerInfrastructure(ctx.config)
-            # Set up signalling. do it here as of no relevance to GUI
-            kill_workers = worker_job(worker, worker.stop)
-            # These first two UNIX & Windows
-            signal.signal(signal.SIGTERM, kill_workers)
-            signal.signal(signal.SIGINT, kill_workers)
+            # These signals are UNIX-only territory, will ValueError here on Windows
+            signal.signal(signal.SIGHUP, kill_workers)
+            # TODO: reload config on SIGUSR1
+            # signal.signal(signal.SIGUSR1, lambda x, y: worker.do_next_tick(worker.reread_config))
+        except ValueError:
+            log.debug("Cannot set all signals -- not available on this platform")
+        if ctx.obj['systemd']:
             try:
-                # These signals are UNIX-only territory, will ValueError here on Windows
-                signal.signal(signal.SIGHUP, kill_workers)
-                # TODO: reload config on SIGUSR1
-                # signal.signal(signal.SIGUSR1, lambda x, y: worker.do_next_tick(worker.reread_config))
-            except ValueError:
-                log.debug("Cannot set all signals -- not available on this platform")
-            if ctx.obj['systemd']:
-                try:
-                    import sdnotify  # A soft dependency on sdnotify -- don't crash on non-systemd systems
-                    n = sdnotify.SystemdNotifier()
-                    n.notify("READY=1")
-                except BaseException:
-                    log.debug("sdnotify not available")
-            worker.run()
-        finally:
-            if ctx.obj['pidfile']:
-                os.unlink(ctx.obj['pidfile'])
+                import sdnotify  # A soft dependency on sdnotify -- don't crash on non-systemd systems
+                n = sdnotify.SystemdNotifier()
+                n.notify("READY=1")
+            except BaseException:
+                log.debug("sdnotify not available")
+        worker.run()
     except errors.NoWorkersAvailable:
         sys.exit(70)  # 70= "Software error" in /usr/include/sysexts.h
+    finally:
+        if ctx.obj['pidfile']:
+            try:
+                os.remove(ctx.obj['pidfile'])
+            except OSError:
+                pass
 
 
 @main.command()
