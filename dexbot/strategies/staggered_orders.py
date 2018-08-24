798b53692fd24d39e3be155d7bcf54c21fcbe26f
Vladimir Kamarzin <vvk@vvk.pp.ru>
2018-08-24 17:11:34 +0500

    Turn bootstrap off when not enough balance
    This may occur in one-sided start when the first order gets filled and
    there are funds available. Intial distribution of new funds will not
    cover full range up to target spread because there will no enough
    balance. This change will allow "big" side to place an additional order.
    
    This new behavior is like just restarting the bot in such situation.

--- dexbot/strategies/staggered_orders.py
+++ dexbot/strategies/staggered_orders.py
@@ -690,6 +690,8 @@ class Strategy(BaseStrategy):
                 self.log.debug('Not enough balance to place_higher_buy_order; need/avail: {}/{}'
                                .format(base_amount, self.base_balance['amount']))
                 place_order = False
+                # Turn bootstrap off, maybe we will be able to place limited order
+                self.bootstrapping = False
             elif allow_partial:
                 self.log.debug('Limiting order amount to available balance: {}'.format(self.base_balance['amount']))
                 amount = self.base_balance['amount'] / price
@@ -774,6 +776,8 @@ class Strategy(BaseStrategy):
                 self.log.debug('Not enough balance to place_lower_sell_order; need/avail: {}/{}'
                                .format(amount, self.quote_balance['amount']))
                 place_order = False
+                # Turn bootstrap off, maybe we will be able to place limited order
+                self.bootstrapping = False
             elif allow_partial:
                 self.log.debug('Limiting order amount to available balance: {}'.format(self.quote_balance['amount']))
                 amount = self.quote_balance['amount']
