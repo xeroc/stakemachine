0e17c358330332e22469b87c893e809704e02374
Vladimir Kamarzin <vvk@vvk.pp.ru>
2018-09-20 18:49:24 +0500

    Allow to control returnOrderId
    This is needed to be able to place orders in non-blocking mode.

--- dexbot/strategies/base.py
+++ dexbot/strategies/base.py
@@ -213,6 +213,9 @@ class StrategyBase(BaseStrategy, Storage, StateMachine, Events):
         # Order expiration time in seconds
         self.expiration = 60 * 60 * 24 * 365 * 5
 
+        # buy/sell actions will return order id by default
+        self.returnOrderId = 'head'
+
         # A private logger that adds worker identify data to the LogRecord
         self.log = logging.LoggerAdapter(
             logging.getLogger('dexbot.per_worker'),
@@ -909,12 +912,12 @@ class StrategyBase(BaseStrategy, Storage, StateMachine, Events):
             return None
 
         # Make sure we have enough balance for the order
-        if self.balance(self.market['base']) < base_amount:
+        if self.returnOrderId and self.balance(self.market['base']) < base_amount:
             self.log.critical("Insufficient buy balance, needed {} {}".format(base_amount, symbol))
             self.disabled = True
             return None
 
-        self.log.info('Placing a buy order for {} {} @ {;.8f}'.format(base_amount, symbol, price)
+        self.log.info('Placing a buy order for {} {} @ {;.8f}'.format(base_amount, symbol, price))
 
         # Place the order
         buy_transaction = self.retry_action(
@@ -923,21 +926,23 @@ class StrategyBase(BaseStrategy, Storage, StateMachine, Events):
             Amount(amount=amount, asset=self.market["quote"]),
             account=self.account.name,
             expiration=self.expiration,
-            returnOrderId="head",
+            returnOrderId=self.returnOrderId,
             fee_asset=self.fee_asset['id'],
             *args,
             **kwargs
         )
 
         self.log.debug('Placed buy order {}'.format(buy_transaction))
-        buy_order = self.get_order(buy_transaction['orderid'], return_none=return_none)
-        if buy_order and buy_order['deleted']:
-            # The API doesn't return data on orders that don't exist
-            # We need to calculate the data on our own
-            buy_order = self.calculate_order_data(buy_order, amount, price)
-            self.recheck_orders = True
-
-        return buy_order
+        if self.returnOrderId:
+            buy_order = self.get_order(buy_transaction['orderid'], return_none=return_none)
+            if buy_order and buy_order['deleted']:
+                # The API doesn't return data on orders that don't exist
+                # We need to calculate the data on our own
+                buy_order = self.calculate_order_data(buy_order, amount, price)
+                self.recheck_orders = True
+            return buy_order
+        else:
+            return True
 
     def place_market_sell_order(self, amount, price, return_none=False, *args, **kwargs):
         """ Places a sell order in the market
@@ -960,7 +965,7 @@ class StrategyBase(BaseStrategy, Storage, StateMachine, Events):
             return None
 
         # Make sure we have enough balance for the order
-        if self.balance(self.market['quote']) < quote_amount:
+        if self.returnOrderId and self.balance(self.market['quote']) < quote_amount:
             self.log.critical("Insufficient sell balance, needed {} {}".format(amount, symbol))
             self.disabled = True
             return None
@@ -974,21 +979,23 @@ class StrategyBase(BaseStrategy, Storage, StateMachine, Events):
             Amount(amount=amount, asset=self.market["quote"]),
             account=self.account.name,
             expiration=self.expiration,
-            returnOrderId="head",
+            returnOrderId=self.returnOrderId,
             fee_asset=self.fee_asset['id'],
             *args,
             **kwargs
         )
 
         self.log.debug('Placed sell order {}'.format(sell_transaction))
-        sell_order = self.get_order(sell_transaction['orderid'], return_none=return_none)
-        if sell_order and sell_order['deleted']:
-            # The API doesn't return data on orders that don't exist, we need to calculate the data on our own
-            sell_order = self.calculate_order_data(sell_order, amount, price)
-            sell_order.invert()
-            self.recheck_orders = True
-
-        return sell_order
+        if self.returnOrderId:
+            sell_order = self.get_order(sell_transaction['orderid'], return_none=return_none)
+            if sell_order and sell_order['deleted']:
+                # The API doesn't return data on orders that don't exist, we need to calculate the data on our own
+                sell_order = self.calculate_order_data(sell_order, amount, price)
+                sell_order.invert()
+                self.recheck_orders = True
+            return sell_order
+        else:
+            return True
 
     def retry_action(self, action, *args, **kwargs):
         """ Perform an action, and if certain suspected-to-be-spurious grapheme bugs occur,
