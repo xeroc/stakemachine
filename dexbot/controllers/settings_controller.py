39e34814cca498f2e147ea7b32ff880cf2e9444a
joelva <joel@codaone.fi>
2018-09-24 15:20:56 +0300

    Add settings_controller file

---
+++ dexbot/controllers/settings_controller.py
@@ -0,0 +1,14 @@
+
+
+class SettingsController:
+
+    def __init__(self):
+        print('SettingsController loaded...')
+
+    @property
+    def nodes(self):
+        """ Defines a list of nodes that are stored in the config file-
+
+            :return: Nodes list
+        """
+        pass
