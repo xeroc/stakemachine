fac65c7ed92aec57c6524c5b4de148e1d104d7ad
joelva <joel@codaone.fi>
2018-09-28 13:29:10 +0300

    Add buttons to SettingsView

--- dexbot/views/settings.py
+++ dexbot/views/settings.py
@@ -1,5 +1,5 @@
-from .ui.settings_window_ui import Ui_settings_dialog
 from dexbot.controllers.settings_controller import SettingsController
+from dexbot.views.ui.settings_window_ui import Ui_settings_dialog
 
 from PyQt5 import QtWidgets
 
@@ -10,8 +10,23 @@ class SettingsView(QtWidgets.QDialog, Ui_settings_dialog):
         super().__init__()
 
         # Initialize view controller
-        controller = SettingsController()
+        self.controller = SettingsController(self)
 
         self.setupUi(self)
 
-        # Add items to list
+        # Initialize list of nodes
+        self.controller.initialize_node_list()
+
+        # Since we are using "parents" for listing the nodes, they are actually "children" for the root item
+        self.root_item = self.nodes_tree_widget.invisibleRootItem()
+
+        # List controls
+        self.add_button.clicked.connect(self.controller.add_node)
+        self.remove_button.clicked.connect(self.controller.remove_node)
+        self.move_up_button.clicked.connect(self.controller.move_up)
+        self.move_down_button.clicked.connect(self.controller.move_down)
+        self.restore_defaults_button.clicked.connect(self.controller.restore_defaults)
+
+        # Tab controls
+        self.button_box.rejected.connect(self.reject)
+        self.button_box.accepted.connect(self.controller.save_settings)
