e4865a4678b231e4f9d9857600ff798cd46b5763
joelva <joel@codaone.fi>
2018-10-11 15:14:05 +0300

    Add csv file reading to details controller

--- dexbot/controllers/worker_details_controller.py
+++ dexbot/controllers/worker_details_controller.py
@@ -1,3 +1,7 @@
+import csv
+
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtGui import QTextCursor
 
 
 class WorkerDetailsController:
@@ -25,3 +29,42 @@ class WorkerDetailsController:
         self.view.strategy_name.setText(self.config.get('module'))
         self.view.market.setText(self.config.get('market'))
         self.view.fee_asset.setText(self.config.get('fee_asset'))
+
+    @staticmethod
+    def populate_table_from_csv(table, file, delimiter=';', first_item_header=True):
+        try:
+            with open(file, 'r') as csv_file:
+                file_reader = csv.reader(csv_file, delimiter=delimiter)
+                rows = list(file_reader)
+        except FileNotFoundError:
+            print('File {} not found'.format(file))
+
+        table.setColumnCount(len(rows[0]))
+
+        # Set headers
+        if first_item_header:
+            headers = rows.pop(0)
+            for header_index, header in enumerate(headers):
+                item = QTableWidgetItem()
+                item.setText(header)
+                table.setHorizontalHeaderItem(header_index, item)
+
+        # Set rows data
+        table.setRowCount(len(rows))
+        for row_index, row in enumerate(rows):
+            for column_index, column in enumerate(row):
+                item = QTableWidgetItem()
+                item.setText(column)
+                table.setItem(row_index, column_index, item)
+
+        return table
+
+    @staticmethod
+    def populate_text_from_file(tab, file):
+        try:
+            tab.text.setPlainText(open(file).read())
+            tab.text.moveCursor(QTextCursor.End)
+
+            return tab
+        except FileNotFoundError:
+            tab.status_label.setText('File \'{}\' not found'.format(file))
