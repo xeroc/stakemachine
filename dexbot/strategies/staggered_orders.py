xeroc/stakemachine

commit d77b708da25f2f0b298fe257c1c7acbbc64b4a03

WIP: neutral mode for staggered orders

dexbot/strategies/staggered_orders.py
@@ -489,6 +489,9 @@ class Strategy(BaseStrategy):
                             own_asset_limit = closest_opposite_order['quote']['amount']
                             self.log.debug('Limiting {} order by opposite order: {} {}'
                                            .format(order_type, own_asset_limit, symbol))
+                        elif self.mode == 'neutral':
+                            # todo: implement
+                            pass
                         elif (self.mode == 'valley' or
                               (self.mode == 'buy_slope' and asset == 'quote') or
                               (self.mode == 'sell_slope' and asset == 'base')):
@@ -767,7 +770,89 @@ class Strategy(BaseStrategy):
                     return
 
         elif self.mode == 'neutral':
-            pass
+            # todo: convert to neutral
+            if asset == 'quote':
+                total_balance = self.quote_total_balance
+                order_type = 'sell'
+            elif asset == 'base':
+                total_balance = self.base_total_balance
+                order_type = 'buy'
+
+            # Get orders and amounts to be compared. Note: orders are sorted from low price to high
+            for order in orders:
+                order_index = orders.index(order)
+                order_amount = order['base']['amount']
+
+                # This check prevents choosing order with index lower than the list length
+                if order_index == 0:
+                    # In case checking the first order, use the same order, but increased by 1 increment
+                    # This allows our closest order amount exceed highest opposite-side order amount
+                    closer_order = order
+                    closer_bound = closer_order['base']['amount'] * (1 + self.increment)
+                else:
+                    closer_order = orders[order_index - 1]
+                    closer_bound = closer_order['base']['amount']
+
+                # This check prevents choosing order with index higher than the list length
+                if order_index + 1 < len(orders):
+                    # Current order is a not furthest order
+                    further_order = orders[order_index + 1]
+                    is_least_order = False
+                else:
+                    # Current order is furthest order
+                    further_order = orders[order_index]
+                    is_least_order = True
+
+                further_bound = further_order['base']['amount'] * (1 + self.increment)
+
+                if (further_bound > order_amount * (1 + self.increment / 10) < closer_bound and
+                        further_bound - order_amount >= order_amount * self.increment / 2):
+                    # Calculate new order size and place the order to the market
+                    new_order_amount = further_bound
+
+                    if is_least_order:
+                        new_orders_sum = 0
+                        amount = order_amount
+                        for o in orders:
+                            amount = amount * (1 + self.increment)
+                            new_orders_sum += amount
+                        # To reduce allocation rounds, increase furthest order more
+                        new_order_amount = order_amount * (total_balance / new_orders_sum) \
+                            * (1 + self.increment * 0.75)
+
+                        if new_order_amount < closer_bound:
+                            """ This is for situations when calculated new_order_amount is not big enough to
+                                allocate all funds. Use partial-increment increase, so we'll got at least one full
+                                increase round.  Whether we will just use `new_order_amount = further_bound`, we will
+                                get less than one full allocation round, thus leaving closest-to-center order not
+                                increased.
+                            """
+                            new_order_amount = closer_bound / (1 + self.increment * 0.2)
+
+                    # Limit sell order to available balance
+                    if asset_balance < new_order_amount - order_amount:
+                        new_order_amount = order_amount + asset_balance['amount']
+                        self.log.info('Limiting new {} order to avail asset balance: {:.8f} {}'
+                                      .format(order_type, new_order_amount, asset_balance['symbol']))
+                    quote_amount = 0
+                    price = 0
+
+                    if asset == 'quote':
+                        price = (order['price'] ** -1)
+                        quote_amount = new_order_amount
+                    elif asset == 'base':
+                        price = order['price']
+                        quote_amount = new_order_amount / price
+
+                    self.log.debug('Cancelling {} order in increase_order_sizes(); mode: {}, amount: {}, price: {:.8f}'
+                                   .format(order_type, self.mode, order_amount, price))
+                    self.cancel(order)
+                    if asset == 'quote':
+                        self.market_sell(quote_amount, price)
+                    elif asset == 'base':
+                        self.market_buy(quote_amount, price)
+                    # Only one increase at a time. This prevents running more than one increment round simultaneously
+                    return
         return None
 
     def check_partial_fill(self, order):
@@ -846,6 +931,8 @@ class Strategy(BaseStrategy):
               (self.mode == 'sell_slope' and asset == 'quote')):
             own_asset_amount = order['base']['amount']
             opposite_asset_amount = own_asset_amount / price
+        elif self.mode == 'neutral':
+            # todo: implement
 
         # Apply limits. Limit order only whether passed limit is less than expected order size
         if own_asset_limit and own_asset_limit < own_asset_amount:
@@ -919,6 +1006,8 @@ class Strategy(BaseStrategy):
         if self.mode == 'mountain' or self.mode == 'buy_slope':
             opposite_asset_amount = order['quote']['amount']
             own_asset_amount = opposite_asset_amount * price
+        if self.mode == 'neutral':
+            # todo: implement
         elif self.mode == 'valley' or self.mode == 'buy_slope':
             own_asset_amount = order['base']['amount']
             opposite_asset_amount = own_asset_amount / price
@@ -990,6 +1079,22 @@ class Strategy(BaseStrategy):
                 previous_amount = amount
                 price = price * (1 + self.increment)
                 amount = amount / (1 + self.increment)
+
+            price = previous_price
+            amount_quote = previous_amount * (self.quote_total_balance / orders_sum) * (1 + self.increment * 0.75)
+
+        if self.mode == 'neutral':
+            previous_price = price
+            orders_sum = 0
+            amount = quote_balance['amount'] * math.sqrt(1 + self.increment)
+            previous_amount = amount
+
+            while price <= self.upper_bound:
+                orders_sum += previous_amount
+                previous_price = price
+                previous_amount = amount
+                price = price * math.sqrt(1 + self.increment)
+                amount = amount / math.sqrt(1 + self.increment)
 
             price = previous_price
             amount_quote = previous_amount * (self.quote_total_balance / orders_sum) * (1 + self.increment * 0.75)
@@ -1082,6 +1187,27 @@ class Strategy(BaseStrategy):
             amount_base = previous_amount * (self.base_total_balance / orders_sum) * (1 + self.increment * 0.75)
             price = previous_price
             amount_quote = amount_base / price
+
+        elif self.mode == 'neutral':
+            previous_price = price
+            orders_sum = 0
+            amount = base_balance['amount'] * sqrt(1 + self.increment)
+            previous_amount = amount
+
+            while price >= self.lower_bound:
+                orders_sum += previous_amount
+                previous_price = price
+                previous_amount = amount
+                price = price / sqrt(1 + self.increment)
+                amount = amount / sqrt(1 + self.increment)
+
+            amount_base = previous_amount * (self.base_total_balance / orders_sum) * (1 + self.increment * 0.75)
+            price = previous_price
+            amount_quote = amount_base / price
+
+        precision = self.market['quote']['precision']
+        amount_quote = int(float(amount_quote) * 10 ** precision) / (10 ** precision)
+
         elif self.mode == 'valley' or self.mode == 'buy_slope':
             orders_count = 0
             while price >= self.lower_bound:
@@ -1096,9 +1222,6 @@ class Strategy(BaseStrategy):
                 allocation to not turn bootstrap off prematurely
             """
             amount_quote = amount_quote / (1 + self.increment / 100)
-
-        precision = self.market['quote']['precision']
-        amount_quote = int(float(amount_quote) * 10 ** precision) / (10 ** precision)
 
         if place_order:
             self.market_buy(amount_quote, price)

commit 0a6a4d94578c93a59da20f1a9c39f20395dde3bd

Fix precision in place_lowest_buy_order()
Regression was introduced in d77b708da25f2f0b298fe257c1c7acbbc64b4a03

dexbot/strategies/staggered_orders.py
@@ -1232,9 +1232,6 @@ class Strategy(BaseStrategy):
             price = previous_price
             amount_quote = amount_base / price
 
-        precision = self.market['quote']['precision']
-        amount_quote = int(float(amount_quote) * 10 ** precision) / (10 ** precision)
-
         elif self.mode == 'valley' or self.mode == 'buy_slope':
             orders_count = 0
             while price >= self.lower_bound:
@@ -1249,6 +1246,9 @@ class Strategy(BaseStrategy):
                 allocation to not turn bootstrap off prematurely
             """
             amount_quote = amount_quote / (1 + self.increment / 100)
+
+        precision = self.market['quote']['precision']
+        amount_quote = int(float(amount_quote) * 10 ** precision) / (10 ** precision)
 
         if place_order:
             self.market_buy(amount_quote, price)