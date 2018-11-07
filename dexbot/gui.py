e26abc7b7ea360094c6b6e65835bfb7c657902c2
joelva <joel@codaone.fi>
2018-11-07 15:30:43 +0200

    Revert "Refactor gui.py variables"
    This reverts commit b016e8418facfd63c398b73abb3870272cd5252a.

--- dexbot/gui.py
+++ dexbot/gui.py
@@ -2,17 +2,16 @@ import sys
 
 from dexbot.config import Config
 from dexbot.controllers.main_controller import MainController
-from dexbot.controllers.wallet_controller import WalletController
-from dexbot.views.create_wallet import CreateWalletView
-from dexbot.views.unlock_wallet import UnlockWalletView
 from dexbot.views.worker_list import MainView
+from dexbot.controllers.wallet_controller import WalletController
+from dexbot.views.unlock_wallet import UnlockWalletView
+from dexbot.views.create_wallet import CreateWalletView
 
 from PyQt5.Qt import QApplication
 from bitshares import BitShares
 
 
 class App(QApplication):
-
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
 
@@ -20,16 +19,16 @@ class App(QApplication):
         bitshares_instance = BitShares(config['node'], num_retries=-1)
 
         # Wallet unlock
-        wallet_controller = WalletController(bitshares_instance)
-        if wallet_controller.wallet_created():
-            unlock_view = UnlockWalletView(wallet_controller)
+        unlock_ctrl = WalletController(bitshares_instance)
+        if unlock_ctrl.wallet_created():
+            unlock_view = UnlockWalletView(unlock_ctrl)
         else:
-            unlock_view = CreateWalletView(wallet_controller)
+            unlock_view = CreateWalletView(unlock_ctrl)
 
         if unlock_view.exec_():
-            bitshares_instance = wallet_controller.bitshares
-            self.main_controller = MainController(bitshares_instance, config)
-            self.main_view = MainView(self.main_controller)
+            bitshares_instance = unlock_ctrl.bitshares
+            self.main_ctrl = MainController(bitshares_instance, config)
+            self.main_view = MainView(self.main_ctrl)
             self.main_view.show()
         else:
             sys.exit()
