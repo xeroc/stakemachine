fbc6fb13e450ef45d0a181429cbd462a1bc57726
hapax.io <39811582+thehapax@users.noreply.github.com>
2018-11-13 00:48:56 -0800

    initial waves feed

---
+++ dexbot/strategies/external_feeds/waves_feed.py
@@ -0,0 +1,9 @@
+import requests
+
+# waves feed - todo
+
+# GET /ticker/{AMOUNT_ASSET}/{PRICE_ASSET}
+ticker_url = https://marketdata.wavesplatform.com/api/ticker/BTC/USD
+
+#Get Symbols Example:
+symbols_url = http://marketdata.wavesplatform.com/api/symbols
