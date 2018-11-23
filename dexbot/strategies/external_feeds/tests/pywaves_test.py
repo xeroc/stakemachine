9f1e1dca854a68106b7ed58a8593c7194e6f1f75
hapax.io <39811582+thehapax@users.noreply.github.com>
2018-11-22 22:03:41 -0800

    add pywaves test

---
+++ dexbot/strategies/external_feeds/tests/pywaves_test.py
@@ -0,0 +1,30 @@
+import pywaves as pw
+
+
+
+if __name__ == '__main__':
+
+        try: 
+                # set the asset pair
+                WAVES_BTC = pw.AssetPair(pw.WAVES, pw.BTC)
+                
+                # get last price and volume
+                print(WAVES_BTC.last(), WAVES_BTC.volume(), sep=' ')
+                
+                # get ticker
+                ticker = WAVES_BTC.ticker()
+                print(ticker['24h_open'])
+                print(ticker['24h_vwap'])
+                
+                # get last 10 trades
+                trades = WAVES_BTC.trades(10)
+                for t in trades:
+	                print(t['buyer'], t['seller'], t['price'], t['amount'], sep=' ')
+	
+                # get last 10 daily OHLCV candles
+                ohlcv = WAVES_BTC.candles(1440, 10)
+                for t in ohlcv:
+	                print(t['open'], t['high'], t['low'], t['close'], t['volume'], sep=' ')
+
+        except Exception as e:
+                print(type(e).__name__, e.args, 'Exchange Error (ignoring)')
