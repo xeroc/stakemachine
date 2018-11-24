dc8c37d79d99dc1dc90256d014744bd30938fe85
hapax.io <39811582+thehapax@users.noreply.github.com>
2018-11-23 22:29:37 -0800

    PEP8

--- dexbot/strategies/external_feeds/price_feed.py
+++ dexbot/strategies/external_feeds/price_feed.py
@@ -1,7 +1,8 @@
-from dexbot.strategies.external_feeds.ccxt_feed import get_ccxt_price
+import dexbot.strategies.external_feeds.ccxt_feed
 from dexbot.strategies.external_feeds.waves_feed import get_waves_price
 from dexbot.strategies.external_feeds.gecko_feed import get_gecko_price
-from dexbot.strategies.external_feeds.process_pair import split_pair, join_pair, filter_prefix_symbol, filter_bit_symbol, get_consolidated_pair, debug
+from dexbot.strategies.external_feeds.process_pair import split_pair, join_pair, filter_prefix_symbol, \
+    filter_bit_symbol, get_consolidated_pair, debug
 import re
 
 
@@ -9,51 +10,43 @@ class PriceFeed:
     """
     price feed class, which handles all data requests for external center price
     """
-                       
-    def __init__(self, exchange, symbol):
-        self._alt_exchanges = ['gecko', 'waves'] # assume all other exchanges are ccxt
-        self._exchange= exchange
-        self._symbol=symbol
-        self._pair= split_pair(symbol)
 
-                   
+    def __init__(self, exchange, symbol):
+        self._alt_exchanges = ['gecko', 'waves']  # assume all other exchanges are ccxt
+        self._exchange = exchange
+        self._symbol = symbol
+        self._pair = split_pair(symbol)
+
     @property
     def symbol(self):
         return self._symbol
 
-    
     @symbol.setter
     def symbol(self, symbol):
         self._symbol = symbol
         self._pair = split_pair(self._symbol)
 
-        
     @property
     def pair(self):
         return self._pair
 
-    
     @pair.setter
-    def pair(self, pair):        
+    def pair(self, pair):
         self._pair = pair
         self._symbol = join_pair(pair)
 
-        
     @property
     def exchange(self):
         return self._exchange
 
-    
     @exchange.setter
     def exchange(self, exchange):
         self._exchange = exchange
 
-
     def filter_symbols(self):
         raw_pair = self._pair
         self._pair = [filter_bit_symbol(j) for j in [filter_prefix_symbol(i) for i in raw_pair]]
-        debug(self._pair)        
-
+        debug(self._pair)
 
     def get_consolidated_price(self):
         """
@@ -70,12 +63,11 @@ class PriceFeed:
             if p1_price and p2_price:
                 center_price = p1_price * p2_price
                 print(original_pair, "price is ", center_price)
-                self.pair = original_pair # put original pair back
+                self.pair = original_pair  # put original pair back
         except Exception as e:
             print(type(e).__name__, e.args, 'Error')
         return center_price
-    
-        
+
     def set_alt_usd_pair(self, type):
         """
         get center price by search and replace for USD with USDT only
@@ -86,29 +78,25 @@ class PriceFeed:
         while i < 2:
             if re.match(r'^USD$', self._pair[i], re.I):
                 alt_usd_pair[i] = re.sub(r'USD', type, self._pair[i])
-            i = i+1
+            i = i + 1
         self._pair = alt_usd_pair
         self._symbol = join_pair(self._pair)
-        
-        
+
     def _get_center_price(self):
         symbol = self._symbol
         price = None
         if self._exchange not in self._alt_exchanges:
-            price = get_ccxt_price(symbol, self._exchange)
+            price = dexbot.strategies.external_feeds.ccxt_feed.get_ccxt_price(symbol, self._exchange)
             debug("use ccxt exchange ", self._exchange, ' symbol ', symbol, ' price:', price)
         elif self._exchange == 'gecko':
             price = get_gecko_price(symbol_=symbol)
-            debug("gecko exchange - ", self._exchange, ' symbol ', symbol, ' price:',  price)
+            debug("gecko exchange - ", self._exchange, ' symbol ', symbol, ' price:', price)
         elif self._exchange == 'waves':
             price = get_waves_price(symbol_=symbol)
-            debug("use waves -", self._exchange, ' symbol ', symbol, ' price:',  price)
+            debug("use waves -", self._exchange, ' symbol ', symbol, ' price:', price)
         return price
-
 
     def get_center_price(self, type):
         if type is not None:
             self.set_alt_usd_pair(type)
-        return  self._get_center_price()
-
-
+        return self._get_center_price()
