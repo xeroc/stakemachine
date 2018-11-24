53810c560ed882b83a084f8f29c020324deea28f
hapax.io <39811582+thehapax@users.noreply.github.com>
2018-11-23 22:36:45 -0800

    PEP8

--- dexbot/strategies/external_feeds/tests/waves_test.py
+++ dexbot/strategies/external_feeds/tests/waves_test.py
@@ -5,27 +5,23 @@ from dexbot.strategies.external_feeds.waves_feed import get_waves_price, get_wav
 This is the unit test for getting external feed data from waves DEX.
 """
 
-
 if __name__ == '__main__':
 
-        symbol = 'BTC/USD'  # quote/base for external exchanges        
-        print(symbol, "=")
-        raw_pair = split_pair(symbol)
-        pair = [filter_bit_symbol(j) for j in [filter_prefix_symbol(i) for i in raw_pair]]
+    symbol = 'BTC/USD'  # quote/base for external exchanges
+    print(symbol, "=")
+    raw_pair = split_pair(symbol)
+    pair = [filter_bit_symbol(j) for j in [filter_prefix_symbol(i) for i in raw_pair]]
 
-        # test symbol and pair options for getting price
-        pair_price = get_waves_price(pair_=pair)
-        if pair_price:
-                print("pair price ", pair_price, sep=":")
+    # test symbol and pair options for getting price
+    pair_price = get_waves_price(pair_=pair)
+    if pair_price:
+        print("pair price ", pair_price, sep=":")
 
-        current_price = get_waves_price(symbol_=symbol)
-        if current_price:
-                print("symbol price ", current_price, sep=":")
+    current_price = get_waves_price(symbol_=symbol)
+    if current_price:
+        print("symbol price ", current_price, sep=":")
 
-                
-        # get entire symbol list
-        print("\n")
-        #        symbol_list = get_waves_symbols()
-        #        print(symbol_list)
-
-
+    # get entire symbol list
+    print("\n")
+    #        symbol_list = get_waves_symbols()
+    #        print(symbol_list)
