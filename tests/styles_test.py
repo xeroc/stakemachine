164fda9eae6c6ce99a36f2399a526dd1b2500383
joelva <joel.vainikka@gmail.com>
2018-11-29 14:32:19 +0200

    Refactor styles tests to separate file

---
+++ tests/styles_test.py
@@ -0,0 +1,12 @@
+from dexbot.styles import green, blue, yellow, red, pink, bold, underline
+
+
+if __name__ == '__main__':
+    # Test each text style
+    print(green("green style test"))
+    print(blue("blue style test"))
+    print(yellow("yellow style test"))
+    print(red("red style test"))
+    print(pink("pink style test"))
+    print(bold("bold style test"))
+    print(underline("underline test"))
