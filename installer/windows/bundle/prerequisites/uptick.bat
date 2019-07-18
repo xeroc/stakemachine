2bdaef6b17a33fd67d40169b02a7792f83fc606b
dominic22 <nexus.domi123@gmail.com>
2019-07-18 22:54:56 +0200

    Add upgrade to uptick installation

--- installer/windows/bundle/prerequisites/uptick.bat
+++ installer/windows/bundle/prerequisites/uptick.bat
@@ -1,6 +1,6 @@
 SET python36=%LocalAppData%\Programs\Python\Python36\python.exe
 SET python36_32=%LocalAppData%\Programs\Python\Python36-32\python.exe
-if exist %python36% %python36% -m pip install uptick
-if exist %python36_32% %python36_32% -m pip install uptick
-rem If both paths are not found try to use pip to install uptick
-if not exist %python36_32% & %python36% pip install uptick
+if exist %python36% %python36% -m pip install uptick --upgrade
+if exist %python36_32% %python36_32% -m pip install uptick --upgrade
+rem If both paths are not found try to use pip to install uptick --upgrade
+if not exist %python36_32% if not exist %python36% pip install uptick --upgrade
