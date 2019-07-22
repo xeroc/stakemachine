079db19592f8e61a34344e80e231c9875024e9d0
Vladimir Kamarzin <vvk@vvk.pp.ru>
2019-07-22 17:41:12 +0500

    Refactor KOTH tests

--- tests/strategies/king_of_the_hill/conftest.py
+++ tests/strategies/king_of_the_hill/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 import time
+import logging
 
 from dexbot.strategies.king_of_the_hill import Strategy
-from bitshares.market import Market
+from dexbot.strategies.base import StrategyBase
 import copy
+
+log = logging.getLogger("dexbot")
 
 MODES = ['both', 'buy', 'sell']
 
@@ -19,19 +22,19 @@ def assets(create_asset):
 
 
 @pytest.fixture(scope='module')
-def account_other(assets, prepare_account):
-    prepare_account({'BASEA': 10000, 'QUOTEA': 100, 'BASEB': 10000, 'QUOTEB': 100, 'TEST': 1000}, account='other')
+def kh_worker_name():
+    """ Fixture to share king_of_the_hill Orders worker name
+    """
+    return 'kh-worker'
 
 
 @pytest.fixture(scope='module')
-def base_account(assets, prepare_account, kh_worker_name):
+def base_account(assets, prepare_account):
     """ Factory to generate random account with pre-defined balances
     """
 
     def func():
-        account = prepare_account(
-            {'BASEA': 10000, 'QUOTEA': 100, 'BASEB': 10000, 'QUOTEB': 100, 'TEST': 1000}, account=kh_worker_name
-        )
+        account = prepare_account({'BASEA': 10000, 'QUOTEA': 100, 'BASEB': 10000, 'QUOTEB': 100, 'TEST': 1000})
         return account
 
     return func
@@ -42,31 +45,6 @@ def account(base_account):
     """ Prepare worker account with some balance
     """
     return base_account()
-
-
-@pytest.fixture(scope='function')
-def other_orders(bitshares, account_other):
-    """ Place some orders from second account
-    """
-    market = Market('QUOTEA/BASEA', bitshares_instance=bitshares)
-    order_ids = []
-    o = market.buy(1, 10, returnOrderId=True, account='other')
-    order_ids.append(o.get('orderid'))
-    o = market.sell(2, 20, returnOrderId=True, account='other')
-    order_ids.append(o.get('orderid'))
-    o = market.buy(1.5, 20, returnOrderId=True, account='other')
-    order_ids.append(o.get('orderid'))
-    yield
-    # if order filled then market.cancel() error
-    market.cancel(order_ids, account='other')
-    time.sleep(1.1)
-
-
-@pytest.fixture(scope='module')
-def kh_worker_name():
-    """ Fixture to share king_of_the_hill Orders worker name
-    """
-    return 'kh-worker'
 
 
 @pytest.fixture(scope='module', params=[('QUOTEA', 'BASEA'), ('QUOTEB', 'BASEB')])
@@ -84,7 +62,7 @@ def config(request, bitshares, account, kh_worker_name):
                 'buy_order_amount': 1.0,
                 'buy_order_size_threshold': 0.0,
                 'fee_asset': 'TEST',
-                'lower_bound': 1,
+                'lower_bound': 1.8,
                 'market': '{}/{}'.format(request.param[0], request.param[1]),
                 'min_order_lifetime': 60,
                 'mode': 'both',
@@ -92,22 +70,43 @@ def config(request, bitshares, account, kh_worker_name):
                 'relative_order_size': False,
                 'sell_order_amount': 2.0,
                 'sell_order_size_threshold': 0.0,
-                'upper_bound': 0.001,
+                'upper_bound': 1.2,
             }
         },
     }
     return config
 
 
+@pytest.fixture(params=MODES)
+def config_variable_modes(request, config, kh_worker_name):
+    """ Test config which tests all modes
+    """
+    worker_name = kh_worker_name
+    config = copy.deepcopy(config)
+    config['workers'][worker_name]['mode'] = request.param
+    return config
+
+
 @pytest.fixture(scope='module')
+def config_other_account(config, base_account, kh_worker_name):
+    """ Config for other account which simulates foreign trader
+    """
+    config = copy.deepcopy(config)
+    worker_name = kh_worker_name
+    config['workers'][worker_name]['account'] = base_account()
+    return config
+
+
+@pytest.fixture
 def base_worker(bitshares, kh_worker_name):
-    """ Fixture to share king_or_the_hill object
+    """ Fixture to create KOTH worker
     """
     worker_name = kh_worker_name
     workers = []
 
     def _base_worker(config):
         worker = Strategy(name=worker_name, config=config, bitshares_instance=bitshares)
+        log.info('Initialized {} on account {}'.format(worker_name, worker.account.name))
         workers.append(worker)
         return worker
 
@@ -126,44 +125,44 @@ def worker(base_worker, config):
     return worker
 
 
-@pytest.fixture(params=MODES)
-def config_variable_modes(request, config, kh_worker_name):
-    """ Test config which tests all modes
+@pytest.fixture
+def worker2(base_worker, config_variable_modes):
+    """ Worker to test all modes
     """
-    worker_name = kh_worker_name
-    config = copy.deepcopy(config)
-    config['workers'][worker_name]['mode'] = request.param
-    return config
+    worker = base_worker(config_variable_modes)
+    return worker
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture
 def orders1(worker):
+    """ Place buy and sell order using worker account
+    """
     worker.place_market_buy_order(1, 100, returnOrderId=True)
     worker.place_market_sell_order(1, 200, returnOrderId=True)
+    yield worker
+    worker.cancel_all_orders()
+    time.sleep(1.1)
 
-    # worker.place_market_buy_order(0.5, 200, returnOrderId=True)
+
+@pytest.fixture
+def other_orders(bitshares, kh_worker_name, config_other_account):
+    """ Place some orders from second account to simulate foreign trader
+    """
+    worker = StrategyBase(name=kh_worker_name, config=config_other_account, bitshares_instance=bitshares)
+    worker.place_market_buy_order(10, 0.9)
+    worker.place_market_buy_order(10, 1)
+    worker.place_market_sell_order(10, 2.1)
+    worker.place_market_sell_order(10, 2)
 
     yield worker
     worker.cancel_all_orders()
     time.sleep(1.1)
 
 
-@pytest.fixture(scope='function')
-def orders2(worker2):
-    worker2.place_market_buy_order(1, 100, returnOrderId=True)
-    worker2.place_market_sell_order(1, 200, returnOrderId=True)
-
-    # worker.place_market_buy_order(0.5, 200, returnOrderId=True)
-
-    yield worker2
-    worker2.cancel_all_orders()
-    time.sleep(1.1)
-
-
 @pytest.fixture
-def worker2(base_worker, config_variable_modes):
-    """ Worker to test all modes
+def other_orders_out_of_bounds(other_orders):
+    """ Extend other_orders by placing additional orders out of bounds
     """
-    print('进入worker2')
-    worker = base_worker(config_variable_modes)
-    return worker
+    worker = other_orders
+    worker.place_market_buy_order(10, worker.worker['upper_bound'] * 1.2)
+    worker.place_market_sell_order(10, worker.worker['lower_bound'] / 1.2)
