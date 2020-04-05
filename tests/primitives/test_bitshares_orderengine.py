2bced964616b43f307c5479d2fdbd49992a176e9
Vladimir Kamarzin <vvk@vvk.pp.ru>
2020-04-05 23:12:03 +0500

    Add test for invert= in place_market_sell_order()

--- tests/primitives/test_bitshares_orderengine.py
+++ tests/primitives/test_bitshares_orderengine.py
@@ -19,3 +19,6 @@ def test_init(worker):
 def test_place_market_sell_order(worker):
     worker.place_market_sell_order(1, 1)
     assert len(worker.own_orders) == 1
+
+    order = worker.place_market_sell_order(1, 10, returnOrderId=True, invert=True)
+    assert order['price'] == 10
